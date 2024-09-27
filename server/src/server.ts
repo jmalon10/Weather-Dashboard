f060109908d0ed452a2fe58a0c45beb6162bc2c2
Jackie Maloney <jmaloney11277@gmail.com>
2024-09-27 09:39:30 -0400

    Define route to serve index.html

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,9 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder MIDDLE WEAR
 app.use(express.static('../client/dist'));
-app.get('*', (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-  });
+
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 
 // TODO: Implement middleware to connect the routes
